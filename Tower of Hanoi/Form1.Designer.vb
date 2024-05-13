1a28a3eb8589ffacaf1a56eff5af8df9fe9f224b
KamiilSW <kam.swierkosz@gmail.com>
2024-05-13 18:20:55 +0100

    Made Tower1Array function which fills ArrayTower1() which checks what layers are filled and what ring is there

--- Tower of Hanoi/Form1.Designer.vb
+++ Tower of Hanoi/Form1.Designer.vb
@@ -22,198 +22,213 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox10 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox11 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox12 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox13 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox14 = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.PictureBox1.Location = New System.Drawing.Point(93, 702)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(310, 24)
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
-        '
-        'PictureBox2
-        '
-        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.PictureBox2.Location = New System.Drawing.Point(553, 702)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(310, 24)
-        Me.PictureBox2.TabIndex = 1
-        Me.PictureBox2.TabStop = False
-        '
-        'PictureBox3
-        '
-        Me.PictureBox3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.PictureBox3.Location = New System.Drawing.Point(1021, 702)
-        Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(310, 24)
-        Me.PictureBox3.TabIndex = 2
-        Me.PictureBox3.TabStop = False
-        '
-        'PictureBox4
-        '
-        Me.PictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.PictureBox4.Location = New System.Drawing.Point(240, 405)
-        Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(14, 302)
-        Me.PictureBox4.TabIndex = 3
-        Me.PictureBox4.TabStop = False
-        '
-        'PictureBox5
-        '
-        Me.PictureBox5.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.PictureBox5.Location = New System.Drawing.Point(696, 405)
-        Me.PictureBox5.Name = "PictureBox5"
-        Me.PictureBox5.Size = New System.Drawing.Size(14, 302)
-        Me.PictureBox5.TabIndex = 4
-        Me.PictureBox5.TabStop = False
-        '
-        'PictureBox6
-        '
-        Me.PictureBox6.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.PictureBox6.Location = New System.Drawing.Point(1167, 405)
-        Me.PictureBox6.Name = "PictureBox6"
-        Me.PictureBox6.Size = New System.Drawing.Size(14, 302)
-        Me.PictureBox6.TabIndex = 5
-        Me.PictureBox6.TabStop = False
-        '
-        'PictureBox7
-        '
-        Me.PictureBox7.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox7.Location = New System.Drawing.Point(107, 683)
-        Me.PictureBox7.Name = "PictureBox7"
-        Me.PictureBox7.Size = New System.Drawing.Size(283, 24)
-        Me.PictureBox7.TabIndex = 6
-        Me.PictureBox7.TabStop = False
-        '
-        'PictureBox8
-        '
-        Me.PictureBox8.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox8.Location = New System.Drawing.Point(120, 662)
-        Me.PictureBox8.Name = "PictureBox8"
-        Me.PictureBox8.Size = New System.Drawing.Size(261, 24)
-        Me.PictureBox8.TabIndex = 7
-        Me.PictureBox8.TabStop = False
-        '
-        'PictureBox9
-        '
-        Me.PictureBox9.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox9.Location = New System.Drawing.Point(134, 644)
-        Me.PictureBox9.Name = "PictureBox9"
-        Me.PictureBox9.Size = New System.Drawing.Size(232, 24)
-        Me.PictureBox9.TabIndex = 8
-        Me.PictureBox9.TabStop = False
-        '
-        'PictureBox10
-        '
-        Me.PictureBox10.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox10.Location = New System.Drawing.Point(146, 623)
-        Me.PictureBox10.Name = "PictureBox10"
-        Me.PictureBox10.Size = New System.Drawing.Size(211, 24)
-        Me.PictureBox10.TabIndex = 9
-        Me.PictureBox10.TabStop = False
-        '
-        'PictureBox11
-        '
-        Me.PictureBox11.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox11.Location = New System.Drawing.Point(159, 605)
-        Me.PictureBox11.Name = "PictureBox11"
-        Me.PictureBox11.Size = New System.Drawing.Size(186, 24)
-        Me.PictureBox11.TabIndex = 10
-        Me.PictureBox11.TabStop = False
-        '
-        'PictureBox12
-        '
-        Me.PictureBox12.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox12.Location = New System.Drawing.Point(175, 584)
-        Me.PictureBox12.Name = "PictureBox12"
-        Me.PictureBox12.Size = New System.Drawing.Size(157, 24)
-        Me.PictureBox12.TabIndex = 11
-        Me.PictureBox12.TabStop = False
-        '
-        'PictureBox13
-        '
-        Me.PictureBox13.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox13.Location = New System.Drawing.Point(188, 564)
-        Me.PictureBox13.Name = "PictureBox13"
-        Me.PictureBox13.Size = New System.Drawing.Size(131, 24)
-        Me.PictureBox13.TabIndex = 12
-        Me.PictureBox13.TabStop = False
-        '
-        'PictureBox14
-        '
-        Me.PictureBox14.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.PictureBox14.Location = New System.Drawing.Point(197, 544)
-        Me.PictureBox14.Name = "PictureBox14"
-        Me.PictureBox14.Size = New System.Drawing.Size(113, 24)
-        Me.PictureBox14.TabIndex = 13
-        Me.PictureBox14.TabStop = False
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1472, 847)
-        Me.Controls.Add(Me.PictureBox14)
-        Me.Controls.Add(Me.PictureBox13)
-        Me.Controls.Add(Me.PictureBox12)
-        Me.Controls.Add(Me.PictureBox11)
-        Me.Controls.Add(Me.PictureBox10)
-        Me.Controls.Add(Me.PictureBox9)
-        Me.Controls.Add(Me.PictureBox8)
-        Me.Controls.Add(Me.PictureBox7)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.PictureBox3)
-        Me.Controls.Add(Me.PictureBox2)
-        Me.Controls.Add(Me.PictureBox6)
-        Me.Controls.Add(Me.PictureBox5)
-        Me.Controls.Add(Me.PictureBox4)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox10, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox11, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox12, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox13, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox14, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        PictureBox4 = New PictureBox()
+        PictureBox5 = New PictureBox()
+        PictureBox6 = New PictureBox()
+        PictureBox7 = New PictureBox()
+        PictureBox8 = New PictureBox()
+        PictureBox9 = New PictureBox()
+        PictureBox10 = New PictureBox()
+        PictureBox11 = New PictureBox()
+        PictureBox12 = New PictureBox()
+        PictureBox13 = New PictureBox()
+        PictureBox14 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox11, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox12, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox13, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox14, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = SystemColors.ActiveCaptionText
+        PictureBox1.Location = New Point(106, 936)
+        PictureBox1.Margin = New Padding(3, 4, 3, 4)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(354, 32)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = SystemColors.ActiveCaptionText
+        PictureBox2.Location = New Point(632, 936)
+        PictureBox2.Margin = New Padding(3, 4, 3, 4)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(354, 32)
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.BackColor = SystemColors.ActiveCaptionText
+        PictureBox3.Location = New Point(1167, 936)
+        PictureBox3.Margin = New Padding(3, 4, 3, 4)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(354, 32)
+        PictureBox3.TabIndex = 2
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.BackColor = SystemColors.ActiveCaptionText
+        PictureBox4.Location = New Point(274, 540)
+        PictureBox4.Margin = New Padding(3, 4, 3, 4)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(16, 403)
+        PictureBox4.TabIndex = 3
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox5
+        ' 
+        PictureBox5.BackColor = SystemColors.ActiveCaptionText
+        PictureBox5.Location = New Point(795, 540)
+        PictureBox5.Margin = New Padding(3, 4, 3, 4)
+        PictureBox5.Name = "PictureBox5"
+        PictureBox5.Size = New Size(16, 403)
+        PictureBox5.TabIndex = 4
+        PictureBox5.TabStop = False
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.BackColor = SystemColors.ActiveCaptionText
+        PictureBox6.Location = New Point(1334, 540)
+        PictureBox6.Margin = New Padding(3, 4, 3, 4)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(16, 403)
+        PictureBox6.TabIndex = 5
+        PictureBox6.TabStop = False
+        ' 
+        ' PictureBox7
+        ' 
+        PictureBox7.BackColor = SystemColors.ActiveCaption
+        PictureBox7.Location = New Point(122, 911)
+        PictureBox7.Margin = New Padding(3, 4, 3, 4)
+        PictureBox7.Name = "PictureBox7"
+        PictureBox7.Size = New Size(323, 32)
+        PictureBox7.TabIndex = 6
+        PictureBox7.TabStop = False
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.BackColor = SystemColors.ActiveCaption
+        PictureBox8.Location = New Point(137, 883)
+        PictureBox8.Margin = New Padding(3, 4, 3, 4)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(298, 32)
+        PictureBox8.TabIndex = 7
+        PictureBox8.TabStop = False
+        ' 
+        ' PictureBox9
+        ' 
+        PictureBox9.BackColor = SystemColors.ActiveCaption
+        PictureBox9.Location = New Point(153, 859)
+        PictureBox9.Margin = New Padding(3, 4, 3, 4)
+        PictureBox9.Name = "PictureBox9"
+        PictureBox9.Size = New Size(265, 32)
+        PictureBox9.TabIndex = 8
+        PictureBox9.TabStop = False
+        ' 
+        ' PictureBox10
+        ' 
+        PictureBox10.BackColor = SystemColors.ActiveCaption
+        PictureBox10.Location = New Point(167, 831)
+        PictureBox10.Margin = New Padding(3, 4, 3, 4)
+        PictureBox10.Name = "PictureBox10"
+        PictureBox10.Size = New Size(241, 32)
+        PictureBox10.TabIndex = 9
+        PictureBox10.TabStop = False
+        ' 
+        ' PictureBox11
+        ' 
+        PictureBox11.BackColor = SystemColors.ActiveCaption
+        PictureBox11.Location = New Point(182, 807)
+        PictureBox11.Margin = New Padding(3, 4, 3, 4)
+        PictureBox11.Name = "PictureBox11"
+        PictureBox11.Size = New Size(213, 32)
+        PictureBox11.TabIndex = 10
+        PictureBox11.TabStop = False
+        ' 
+        ' PictureBox12
+        ' 
+        PictureBox12.BackColor = SystemColors.ActiveCaption
+        PictureBox12.Location = New Point(200, 779)
+        PictureBox12.Margin = New Padding(3, 4, 3, 4)
+        PictureBox12.Name = "PictureBox12"
+        PictureBox12.Size = New Size(179, 32)
+        PictureBox12.TabIndex = 11
+        PictureBox12.TabStop = False
+        ' 
+        ' PictureBox13
+        ' 
+        PictureBox13.BackColor = SystemColors.ActiveCaption
+        PictureBox13.Location = New Point(215, 752)
+        PictureBox13.Margin = New Padding(3, 4, 3, 4)
+        PictureBox13.Name = "PictureBox13"
+        PictureBox13.Size = New Size(150, 32)
+        PictureBox13.TabIndex = 12
+        PictureBox13.TabStop = False
+        ' 
+        ' PictureBox14
+        ' 
+        PictureBox14.BackColor = SystemColors.ActiveCaption
+        PictureBox14.Location = New Point(227, 723)
+        PictureBox14.Margin = New Padding(3, 4, 3, 4)
+        PictureBox14.Name = "PictureBox14"
+        PictureBox14.Size = New Size(129, 32)
+        PictureBox14.TabIndex = 13
+        PictureBox14.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1682, 1055)
+        Controls.Add(PictureBox14)
+        Controls.Add(PictureBox13)
+        Controls.Add(PictureBox12)
+        Controls.Add(PictureBox11)
+        Controls.Add(PictureBox10)
+        Controls.Add(PictureBox9)
+        Controls.Add(PictureBox8)
+        Controls.Add(PictureBox7)
+        Controls.Add(PictureBox1)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox6)
+        Controls.Add(PictureBox5)
+        Controls.Add(PictureBox4)
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox10, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox11, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox12, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox13, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox14, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
 
     End Sub
 
--- Tower of Hanoi/Form1.vb
+++ Tower of Hanoi/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Runtime.InteropServices 'Need to import...
+﻿Imports System.Runtime.InteropServices
 
 Public Class Form1
 
@@ -11,8 +11,7 @@ Public Class Form1
 
     Dim x As Integer
     Dim y As Integer
-    Private Sub PictureBox14_MouseDown(sender As Object, e As EventArgs) Handles PictureBox14.MouseDown
-        Dim MouseIsDown As Boolean = True
+    Private Sub PictureBox14_MouseDown(sender As Object, e As EventArgs)
         Do
             x = MousePosition.X - 230
             y = MousePosition.Y - 240
@@ -21,20 +20,200 @@ Public Class Form1
 
         Loop Until LeftMouseIsDown() = False
 
-        PictureBox14.Location = New Point(x, y)
+        PictureBox14.Location = New Point(x, y) 'Moves PictureBox to base of Tower 2
+
         Tower2()
         Exit Sub
 
     End Sub
+    Dim ArrayTower1() As String
+    Dim ArrayTower2() As String
+    Dim ArrayTower3() As String
 
+    Function Tower1Array() As String
+        For i = 1 To 8
+            If i = 1 And PictureBox14.Location.Y = 725 Then
+                ArrayTower1(i) = "814"
+            ElseIf i = 1 And PictureBox14.Location.Y = 752 Then
+                ArrayTower1(i) = "714"
+
+            ElseIf i = 1 And PictureBox14.Location.Y = 779 Then
+                ArrayTower1(i) = "614"
+
+            ElseIf i = 1 And PictureBox14.Location.Y = 806 Then
+                ArrayTower1(i) = "514"
+
+            ElseIf i = 1 And PictureBox14.Location.Y = 833 Then
+                ArrayTower1(i) = "414"
+
+            ElseIf i = 1 And PictureBox14.Location.Y = 860 Then
+                ArrayTower1(i) = "314"
+
+            ElseIf i = 1 And PictureBox14.Location.Y = 887 Then
+                ArrayTower1(i) = "214"
+
+            ElseIf i = 1 And PictureBox14.Location.Y = 914 Then
+                ArrayTower1(i) = "114"
+
+            ElseIf i = 2 And PictureBox13.Location.Y = 725 Then
+                ArrayTower1(i) = "813"
+
+            ElseIf i = 2 And PictureBox13.Location.Y = 752 Then
+                ArrayTower1(i) = "713"
+
+            ElseIf i = 2 And PictureBox13.Location.Y = 779 Then
+                ArrayTower1(i) = "613"
+
+            ElseIf i = 2 And PictureBox13.Location.Y = 806 Then
+                ArrayTower1(i) = "513"
+
+            ElseIf i = 2 And PictureBox13.Location.Y = 833 Then
+                ArrayTower1(i) = "413"
+
+            ElseIf i = 2 And PictureBox13.Location.Y = 860 Then
+                ArrayTower1(i) = "313"
+
+            ElseIf i = 2 And PictureBox13.Location.Y = 887 Then
+                ArrayTower1(i) = "213"
+
+            ElseIf i = 2 And PictureBox13.Location.Y = 914 Then
+                ArrayTower1(i) = "113"
+
+            ElseIf i = 3 And PictureBox12.Location.Y = 725 Then
+                ArrayTower1(i) = "812"
+
+            ElseIf i = 3 And PictureBox12.Location.Y = 752 Then
+                ArrayTower1(i) = "712"
+
+            ElseIf i = 3 And PictureBox12.Location.Y = 779 Then
+                ArrayTower1(i) = "612"
+
+            ElseIf i = 3 And PictureBox12.Location.Y = 806 Then
+                ArrayTower1(i) = "512"
+
+            ElseIf i = 3 And PictureBox12.Location.Y = 833 Then
+                ArrayTower1(i) = "412"
+
+            ElseIf i = 3 And PictureBox12.Location.Y = 860 Then
+                ArrayTower1(i) = "312"
+
+            ElseIf i = 3 And PictureBox12.Location.Y = 887 Then
+                ArrayTower1(i) = "212"
+
+            ElseIf i = 3 And PictureBox12.Location.Y = 914 Then
+                ArrayTower1(i) = "112"
+
+            ElseIf i = 4 And PictureBox11.Location.Y = 725 Then
+                ArrayTower1(i) = "811"
+
+            ElseIf i = 4 And PictureBox11.Location.Y = 752 Then
+                ArrayTower1(i) = "711"
+
+            ElseIf i = 4 And PictureBox11.Location.Y = 779 Then
+                ArrayTower1(i) = "611"
+
+            ElseIf i = 4 And PictureBox11.Location.Y = 806 Then
+                ArrayTower1(i) = "511"
+
+            ElseIf i = 4 And PictureBox11.Location.Y = 833 Then
+                ArrayTower1(i) = "411"
+
+            ElseIf i = 4 And PictureBox11.Location.Y = 860 Then
+                ArrayTower1(i) = "311"
+
+            ElseIf i = 4 And PictureBox11.Location.Y = 887 Then
+                ArrayTower1(i) = "211"
+
+            ElseIf i = 4 And PictureBox11.Location.Y = 914 Then
+                ArrayTower1(i) = "111"
+
+            ElseIf i = 5 And PictureBox10.Location.Y = 725 Then
+                ArrayTower1(i) = "810"
+
+            ElseIf i = 5 And PictureBox10.Location.Y = 752 Then
+                ArrayTower1(i) = "710"
+
+            ElseIf i = 5 And PictureBox10.Location.Y = 779 Then
+                ArrayTower1(i) = "610"
+
+            ElseIf i = 5 And PictureBox10.Location.Y = 806 Then
+                ArrayTower1(i) = "510"
+
+            ElseIf i = 5 And PictureBox10.Location.Y = 833 Then
+                ArrayTower1(i) = "410"
+
+            ElseIf i = 5 And PictureBox10.Location.Y = 860 Then
+                ArrayTower1(i) = "310"
+
+            ElseIf i = 5 And PictureBox10.Location.Y = 887 Then
+                ArrayTower1(i) = "210"
+
+            ElseIf i = 5 And PictureBox10.Location.Y = 914 Then
+                ArrayTower1(i) = "110"
+
+            ElseIf i = 6 And PictureBox9.Location.Y = 725 Then
+                ArrayTower1(i) = "89"
+
+            ElseIf i = 6 And PictureBox9.Location.Y = 752 Then
+                ArrayTower1(i) = "79"
+
+            ElseIf i = 6 And PictureBox9.Location.Y = 779 Then
+                ArrayTower1(i) = "69"
+
+            ElseIf i = 6 And PictureBox9.Location.Y = 806 Then
+                ArrayTower1(i) = "59"
+
+            ElseIf i = 6 And PictureBox9.Location.Y = 833 Then
+                ArrayTower1(i) = "49"
+
+            ElseIf i = 6 And PictureBox9.Location.Y = 860 Then
+                ArrayTower1(i) = "39"
+
+            ElseIf i = 6 And PictureBox9.Location.Y = 887 Then
+                ArrayTower1(i) = "29"
+
+            ElseIf i = 6 And PictureBox9.Location.Y = 914 Then
+                ArrayTower1(i) = "19"
+
+            ElseIf i = 7 And PictureBox8.Location.Y = 725 Then
+                ArrayTower1(i) = "88"
+
+            ElseIf i = 7 And PictureBox8.Location.Y = 752 Then
+                ArrayTower1(i) = "78"
+
+            ElseIf i = 7 And PictureBox8.Location.Y = 779 Then
+                ArrayTower1(i) = "68"
+
+            ElseIf i = 7 And PictureBox8.Location.Y = 806 Then
+                ArrayTower1(i) = "58"
+
+            ElseIf i = 7 And PictureBox8.Location.Y = 833 Then
+                ArrayTower1(i) = "48"
+
+            ElseIf i = 7 And PictureBox8.Location.Y = 860 Then
+                ArrayTower1(i) = "38"
+
+            ElseIf i = 7 And PictureBox8.Location.Y = 887 Then
+                ArrayTower1(i) = "28"
+
+            ElseIf i = 7 And PictureBox8.Location.Y = 914 Then
+                ArrayTower1(i) = "18"
+            End If
+
+        Next
+        Return True
+    End Function
     Function Tower2() As Boolean
         If (PictureBox14.Location.X > 534 And PictureBox14.Location.X < 880) Or (PictureBox14.Location.Y < 734 And PictureBox14.Location.Y > 376) Then
             PictureBox14.Location = New Point(648, 683)
             Return True
+
         Else
             PictureBox14.Location = New Point(197, 544)
             Return False
+
         End If
+
     End Function
 
 End Class
